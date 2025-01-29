4970f9ff318c8d577be6c96ea4580f3dd2c93bbc
Eryck Santos <dev.erycksantos@gmail.com>
2025-01-29 12:19:57 -0300

    Initial project sketch. Functions and conditional structure created.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Escolha uma opção");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit();  break;
+    }
+}
+
+static void Open()
+{
+
+}
+
+static void Edit()
+{
+
+}
